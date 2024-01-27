fbd88547cc08f6942d254f51a59fc71db7f6eb4a
Evgeniy Budaev <budaev.e@gmail.com>
2024-01-27 15:50:12 +0300

    feat: username

--- app/pages/mainPage/MainPage.tsx
+++ app/pages/mainPage/MainPage.tsx
@@ -1,26 +1,32 @@
 "use client";
 
-import type { FC } from "react";
-import { useTelegram } from "@/app/shared/hooks";
+import {FC, useEffect} from "react";
+import {useTelegram} from "@/app/shared/hooks";
 
 export const MainPage: FC = () => {
-  const { tg, user, queryId } = useTelegram();
+    const {tg, user, queryId} = useTelegram();
 
-  return (
-    <div>
-      <div>Привет!</div>
-      <div>User: {user?.username}</div>
-      <div>
-        <pre>{JSON.stringify(user, null, 2)}</pre>
-      </div>
-      <div>queryId:</div>
-      <div>
-        <pre>{JSON.stringify(queryId, null, 2)}</pre>
-      </div>
-      <div>tg.initData:</div>
-      <div>
-        <pre>{JSON.stringify(tg?.initData, null, 2)}</pre>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        tg?.ready();
+    }, [tg]);
+
+    return (
+        <div>
+            <div>Привет!</div>
+            <div>User: {user?.username}</div>
+            <div>
+                <pre>{JSON.stringify(user, null, 2)}</pre>
+            </div>
+            <div>queryId:</div>
+            <div>
+                <pre>{JSON.stringify(queryId, null, 2)}</pre>
+            </div>
+            <div>tg.initData:</div>
+            <div>
+                <pre>{JSON.stringify(tg?.initData, null, 2)}</pre>
+            </div>
+            =============================
+            <div>User: {tg?.initDataUnsafe?.user?.username}</div>
+        </div>
+    );
 };
