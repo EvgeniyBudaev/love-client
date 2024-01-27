1dd01a22e75bb7a9ee45fcd56ff4678811f2ca76
Evgeniy Budaev <budaev.e@gmail.com>
2024-01-27 15:34:22 +0300

    feat: images domains

--- next.config.js
+++ next.config.js
@@ -2,9 +2,14 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, "app/styles")],
-  },
+    sassOptions: {
+        includePaths: [path.join(__dirname, "app/styles")],
+    },
+    images: {
+        domains: [
+            "imaginative-bombolone-f1096b.netlify.app",
+        ],
+    },
 };
 
 module.exports = nextConfig;
