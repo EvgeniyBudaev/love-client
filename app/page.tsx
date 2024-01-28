bb837a538771f386a1ecc9bf3850436d8a62452c
Evgeniy Budaev <budaev.e@gmail.com>
2024-01-28 14:19:05 +0300

    feat: test class Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import "./home.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h2 className="Home">HELLO WORLD</h2>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
