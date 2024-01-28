0194f67ea86470b5c0b6cccbe9a3d2073f17cd6a
Evgeniy Budaev <budaev.e@gmail.com>
2024-01-28 16:50:38 +0300

    feat: NavLink

--- app/shared/components/footer/Footer.tsx
+++ app/shared/components/footer/Footer.tsx
@@ -1,34 +1,39 @@
 import Link from "next/link";
-import type { FC } from "react";
-import { ERoutes } from "@/app/shared/enums";
-import { Avatar } from "@/app/uikit/components/avatar";
-import { createPath } from "@/app/shared/utils";
+import type {FC} from "react";
+import {ERoutes} from "@/app/shared/enums";
+import {NavLink} from "@/app/shared/components/navLink";
+import {Avatar} from "@/app/uikit/components/avatar";
+import {Icon} from "@/app/uikit/components/icon";
+import {createPath} from "@/app/shared/utils";
 import "./Footer.scss";
-import { Icon } from "@/app/uikit/components/icon";
 
 export const Footer: FC = () => {
   return (
     <div className="Footer">
       <div className="Footer-Item">
-        <Link
+        <NavLink
+          activeClassName="Footer-Link__isActive"
           href={createPath({
             route: ERoutes.Root,
           })}
         >
-          <Icon type="Search" />
-        </Link>
+          <Icon type="Search"/>
+        </NavLink>
       </div>
       <div className="Footer-Item">
-        <Link
+        <NavLink
+          activeClassName="Footer-Link__isActive"
           href={createPath({
             route: ERoutes.Profile,
           })}
         >
-          <Avatar
-            size={46}
-            image="https://img.freepik.com/premium-photo/photo-portrait-of-pretty-girl-in-orange-sweater-smiling-isolated-on-bright-teal-color-background_908985-11469.jpg"
-          />
-        </Link>
+          <div className="Footer-Avatar">
+            <Avatar
+              size={32}
+              image="https://img.freepik.com/premium-photo/photo-portrait-of-pretty-girl-in-orange-sweater-smiling-isolated-on-bright-teal-color-background_908985-11469.jpg"
+            />
+          </div>
+        </NavLink>
       </div>
     </div>
   );
