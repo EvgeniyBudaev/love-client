93b7fcca1db238a46fe260506ca433af59267165
Evgeniy Budaev <budaev.e@gmail.com>
2024-01-28 13:51:31 +0300

    feat: output export

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "imaginative-bombolone-f1096b.netlify.app",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "img.freepik.com",
-        port: "",
-      },
-    ],
-  },
+    output: 'export',
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "imaginative-bombolone-f1096b.netlify.app",
+                port: "",
+            },
+            {
+                protocol: "https",
+                hostname: "img.freepik.com",
+                port: "",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
