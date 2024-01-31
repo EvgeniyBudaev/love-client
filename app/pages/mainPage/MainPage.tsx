b78736e3dc76f5286551f06aef9dba5533e9f365
Evgeniy Budaev <budaev.e@gmail.com>
2024-01-31 09:46:09 +0300

    fix: username

--- app/pages/mainPage/MainPage.tsx
+++ app/pages/mainPage/MainPage.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import Link from "next/link";
-import { FC } from "react";
-import { useTelegram } from "@/app/shared/hooks";
-import { createPath } from "@/app/shared/utils";
-import { ERoutes } from "@/app/shared/enums";
+import {FC} from "react";
+import {useTelegram} from "@/app/shared/hooks";
+import {createPath} from "@/app/shared/utils";
+import {ERoutes} from "@/app/shared/enums";
 
 export const MainPage: FC = () => {
-  const { tg, user, queryId } = useTelegram();
+  const {tg, user, queryId} = useTelegram();
 
   return (
     <div>
       <div>Привет!</div>
+      <div>{user?.username}</div>
       <Link
         href={createPath({
           route: ERoutes.ProfileAdd,
